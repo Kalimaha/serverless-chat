a40245b7b12a7d7c0e4f85c0500df5b9b6fba9ff
Guido Barbaglia <guido.barbaglia@mebank.com.au>
2020-06-01 10:09:14 +1000

    G22 - WIP

--- functions/messages/create/controllers.go
+++ functions/messages/create/controllers.go
@@ -3,16 +3,28 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/apigateway"
+	"github.com/aws/aws-sdk-go/service/apigateway/apigatewayiface"
+	"github.com/aws/aws-sdk-go/service/apigatewaymanagementapi"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 	guuid "github.com/google/uuid"
 	"time"
 )
 
 type LambdaRequest struct {
 	Data string `json:"data"`
+}
+
+type Pippo struct {
+	PrimaryKey string
+	Data bool
 }
 
 func CreateMessage(request events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
@@ -26,12 +38,81 @@ func CreateMessage(request events.APIGatewayWebsocketProxyRequest) (events.APIGa
 		returnFormattedError(err)
 	}
 
-	body, err := json.Marshal(message)
+	//body, err := json.Marshal(message)
+	//if err != nil {
+	//	returnFormattedError(err)
+	//}
+
+	// Retrieve active users
+	conditionOne := expression.Name("PrimaryKey").BeginsWith("USER_")
+	conditionTwo := expression.Name("Data").Equal(expression.Value(true))
+	//expr, err := expression.NewBuilder().WithCondition(conditionOne.And(conditionTwo)).Build()
+
+	//expr, err := expression.NewBuilder().WithCondition(conditionOne).Build()
+
+	//filt := expression.Name("PrimaryKey").BeginsWith("USER_")
+	proj := expression.NamesList(expression.Name("PrimaryKey"), expression.Name("Data"))
+	//expr, err := expression.NewBuilder().WithFilter(filt).WithProjection(proj).Build()
+
+	expr, err := expression.NewBuilder().WithFilter(conditionOne.And(conditionTwo)).WithProjection(proj).Build()
 	if err != nil {
 		returnFormattedError(err)
 	}
 
+	queryParams := &dynamodb.ScanInput{
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		FilterExpression:          expr.Filter(),
+		ProjectionExpression:      expr.Projection(),
+		TableName:                 aws.String(TableName),
+	}
+
+	queryResult, err := dbSession().Scan(queryParams)
+	if err != nil {
+		returnFormattedError(err)
+	}
+
+	queryOut := make([]Pippo, len(queryResult.Items))
+	for i, v := range queryResult.Items {
+		tmp := Pippo{}
+		dynamodbattribute.UnmarshalMap(v, &tmp)
+		queryOut[i] = tmp
+	}
+
+
+	sess := session.Must(session.NewSession())
+	svc := apigatewaymanagementapi.New(sess)
+
+	log.Print(fmt.Sprintf("Active connections: %d", len(queryOut)))
+	for _, v := range queryOut {
+		connectionId := v.PrimaryKey[5:len(v.PrimaryKey)]
+		connectionUrl := fmt.Sprintf("https://lquoki7y58.execute-api.ap-southeast-2.amazonaws.com/test/@connections/%s", connectionId)
+		log.Print(fmt.Sprintf("Connection ID: %s", connectionId))
+		log.Print(fmt.Sprintf("Connection URL: %s", connectionUrl))
+		connectionString := aws.String(connectionUrl)
+		params := apigatewaymanagementapi.PostToConnectionInput{
+			ConnectionId: connectionString,
+			Data: []byte("{\"hallo\": \"world\"}"),
+		}
+		output, err := svc.PostToConnection(&params)
+		if err != nil {
+			returnFormattedError(err)
+		}
+		log.Print(fmt.Sprintf("PostToConnection OUTPUT: %s", output.String()))
+	}
+
+
+	body, err := json.Marshal(queryOut)
+	if err != nil {
+		returnFormattedError(err)
+	}
 	return events.APIGatewayProxyResponse{Body: string(body), StatusCode: 201}, nil
+}
+
+func apiGatewaySession() (apiGatewaySession apigatewayiface.APIGatewayAPI) {
+	sess := session.Must(session.NewSession())
+
+	return apigateway.New(sess)
 }
 
 func dbSession() (dbSession dynamodbiface.DynamoDBAPI) {
